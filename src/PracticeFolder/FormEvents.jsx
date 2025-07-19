be51115ae7525fc2b4cdb3ffb4d1f874595bcadb
Macha-Vasu <macha.vasu@outlook.com>
2025-07-19 20:55:17 +0530

    code update

---
+++ src/PracticeFolder/FormEvents.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+<form id="myForm">
+    <label for="nameInput">Name</label>
+    <input type="text" placeholder='Enter your name'></input>
+    <button type="submit">Submit</button>
+
+    </form>
+
+    export default formEvents;
