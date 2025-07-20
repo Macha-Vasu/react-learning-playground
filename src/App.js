0e3884cc689eca9e4141e13a3a10d8fdafb1894f
Macha-Vasu <macha.vasu@outlook.com>
2025-07-20 21:02:27 +0530

    Shopping

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 import './App.css';
+import MainPage from './pages/MainPage';
+
 
 function App() {
   return (
     <div className="App">
       <h1>React Learning Playground!!</h1>
+      <MainPage />
     </div>
   );
 }
+
+
 
 export default App;
