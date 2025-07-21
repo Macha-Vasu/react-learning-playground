1a41f94af6bd8ebae2764db510cb777824adb4ab
Macha-Vasu <macha.vasu@outlook.com>
2025-07-21 14:42:09 +0530

    update

--- src/Shopping/Components/Header.jsx
+++ src/Shopping/Components/Header.jsx
@@ -1,30 +1,35 @@
-import React from "react";
-import "./Header.css";
+import React from 'react'
 
 const Header = () => {
   return (
-    <div className="headerSection">
-      <div className="left">Shopping Mall</div>
+    <div className='headerSection'>
+        <div className="left">
+            <div className="title">
+                <h2>Shopping Mall</h2>
+            </div>
+        </div>
+        <div className="center">
+            <ul>
+                <li>Woman</li>
+                <li>Men</li>
+                <li>Children</li>
+                <li>Beauty</li>
+            </ul>
 
-      <div className="center">
-        <ul>
-          <li>Woman</li>
-          <li>Men</li>
-          <li>Children</li>
-          <li>Beauty</li>
-        </ul>
-      </div>
-
-      <div className="search">
-        <input type="text" placeholder="Search..." />
-      </div>
-
-      <div className="right">
-        <div>Signin / Signup</div>
-        <div>Cart</div>
-      </div>
+        </div>
+        <div className="search">
+            <input type="text" placeholder='Search...' />
+        </div>
+        <div className="right">
+            <div className="signin">
+                Signin / Signup
+            </div>
+            <div className="cart">
+                Cart
+            </div>
+        </div>
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
