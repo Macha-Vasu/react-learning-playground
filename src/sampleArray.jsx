15c8fed934f6b8103b826f39b9a1869a17afa69f
Macha-Vasu <macha.vasu@outlook.com>
2025-07-22 14:36:46 +0530

    update

---
+++ src/sampleArray.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+let fruits = ["apple", "banana", "orange", "grape"]
+
+let players = [
+    {
+    name: "apple",
+    name: "banana",
+    name: "orange",
+    name: "grape"
+}
+]
+
+const sampleArray = () => {
+    return (
+        <div>
+            {fruits.map((fruit, index) =>
+                <p key={index}>{fruit}</p>
+            )}
+        </div>
+    );
+}    
