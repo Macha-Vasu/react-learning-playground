37c8bd3c30ec62076cfd4bb893474c9115f408a1
Macha-Vasu <macha.vasu@outlook.com>
2025-07-24 20:05:04 +0530

    code update

--- src/sampleArray.jsx
+++ src/sampleArray.jsx
@@ -27,27 +27,11 @@ const SampleArray = () => {
            } )
            
         
-        }
-function Dashboard() {
-  const { data, isLoading, error } = useQuery('fetchData', fetchDashboardData);
-  
-  if (isLoading) return <Spinner />;
-  if (error) return <Error message={error.message} />;
-
-  return (
-    <div>
-      <Header />
-      <Sidebar />
-      <MainContent data={data} />
-    </div>
-  );
-}
-
-
-
+           
+       
         </div>
-    );
-    
+
+
 }    
 
 export default SampleArray;
